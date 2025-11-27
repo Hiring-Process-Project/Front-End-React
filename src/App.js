89744b1da1272f6bf6676bd6316f3475d45ad78b
SofiaKal <128231824+SofiaKal@users.noreply.github.com>
2025-11-27 23:17:31 +0200

    button for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import MyGridLayout from './components/MyGridLayout';
-import OrgStructureApp from './components/OrgStructureApp';
+import React, { useState } from "react";
+import "./App.css";
+import MyGridLayout from "./components/MyGridLayout";
+import OrgStructureApp from "./components/OrgStructureApp";
 
 function App() {
+  const [page, setPage] = useState("org");
+
   return (
-    <div>
-      <OrgStructureApp />
-    </div>
+    <>
+      {page === "org" ? (
+        <OrgStructureApp onSwitchPage={() => setPage("grid")} />
+      ) : (
+        <MyGridLayout onSwitchPage={() => setPage("org")} />
+      )}
+    </>
   );
 }
-
-export default App;
